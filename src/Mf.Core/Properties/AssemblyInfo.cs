3e00a3a4af5d4d4f3b32224badc56fbc1bcce3bd
Nikita Ermakov <neketa.erma13@gmail.com>
2023-09-27 12:12:10 +0300

    Update AssemblyInfo.cs metadata
    The AssemblyInfo.cs file was updated. Specifically, a character was added to delineate a note or comment in the file. This makes the distinction between code and comments clearer, facilitating easier comprehension for development teams that might need to review or update this code in the future.

--- src/Mf.Core/Properties/AssemblyInfo.cs
+++ src/Mf.Core/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Mf.Core")]
 [assembly: AssemblyTrademark("")]
-
+//1
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
