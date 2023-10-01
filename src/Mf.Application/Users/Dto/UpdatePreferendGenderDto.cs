77143f676295b8b8073138b6d97bcaf5f5599ef1
Vadim <wexordeker@gmail.com>
2023-10-01 17:56:53 +0300

    [MF-05] add new checks in method UpdatePreferendGender

--- src/Mf.Application/Users/Dto/UpdatePreferendGenderDto.cs
+++ src/Mf.Application/Users/Dto/UpdatePreferendGenderDto.cs
@@ -1,13 +1,40 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Mf.Users.Dto;
 
-public class UpdatePreferendGenderDto
+public class UpdatePreferendGenderDto : IValidatableObject
 {
     [Required(ErrorMessage = "Поле 'UserId' обязательно для заполнения.")]
     public string UserId { get; set; }
-    
+
     [Required(ErrorMessage = "Поле 'NewPreferendGender' обязательно для заполнения.")]
     public string NewPreferendGender { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        string[] genders = { "male", "female", "other" };
+        
+        if (string.IsNullOrWhiteSpace(NewPreferendGender))
+        {
+            yield return new ValidationResult(
+                "Поле 'NewPreferendGender' не может быть пустым или содержать только пробелы.",
+                new[] { nameof(NewPreferendGender) });
+        }
+
+        if (string.IsNullOrWhiteSpace(UserId))
+        {
+            yield return new ValidationResult("Поле 'UserId' не может быть пустым или содержать только пробелы.",
+                new[] { nameof(UserId) });
+        }
+        
+        if (!genders.Contains(NewPreferendGender))
+        {
+            yield return new ValidationResult(
+                "Поле 'NewPreferendGender' может содержать только male, female или other.",
+                new[] { nameof(NewPreferendGender) });
+        }
+    }
 }
